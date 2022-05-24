67be20afd30b820bda2664c18c36d750fdca12eb
Beata Huszar <beata.huszar@playngo.com>
2022-05-24 17:17:13 +0200

    some refactor

--- src/App.test.js
+++ src/App.test.js
@@ -2,120 +2,97 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import userEvent from "@testing-library/user-event";
 
-test('inputs should be initially empty', () => {
-  render(<App />);
-  const emailInputElement = screen.getByRole("textbox");
-  const passwordInputElement = screen.getByLabelText("Password");
-  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i);
-  expect(emailInputElement.value).toBe("");
-  expect(passwordInputElement.value).toBe("");
-  expect(confirmPasswordInputElement.value).toBe("");
-});
-
-test('should be able to type an email', () => {
-  render(<App />);
+const typeIntoForm = ({email, password, confirmPassword}) => {
   const emailInputElement = screen.getByRole("textbox", {
     name: /email/i
   });
-  userEvent.type(emailInputElement, "selena@gmail.com");
-  expect(emailInputElement.value).toBe("selena@gmail.com")
-});
-
-test('should be able to type a password', () => {
-  render(<App />);
   const passwordInputElement = screen.getByLabelText("Password");
-  userEvent.type(passwordInputElement, "pwd");
-  expect(passwordInputElement.value).toBe("pwd")
-});
-
-test('should be able to type a confirm-password', () => {
-  render(<App />);
-  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i);
-  userEvent.type(confirmPasswordInputElement, "cpwd");
-  expect(confirmPasswordInputElement.value).toBe("cpwd")
-});
-
-test('should show email error message on invalid email', () => {
-  render(<App />);
-  const emailErrorelement = screen.queryByText(/the email you input is invalid/i);
-  const emailInputElement = screen.getByRole("textbox", {
-    name: /email/i
-  });
-  const submitBtnElement = screen.getByRole("button", {
-    name: /submit/i
-  });
-
-  expect(emailErrorelement).not.toBeInTheDocument();
-  userEvent.type(emailInputElement, "blablamail.com");
-  userEvent.click(submitBtnElement);
-  const emailErrorelement2 = screen.queryByText(/the email you input is invalid/i);
-
-  expect(emailErrorelement2).toBeInTheDocument();
-});
-
-test('should show password error if password is less than 5 characters', () => {
-  render(<App />);
-  const emailInputElement = screen.getByRole("textbox", {
-    name: /email/i
-  });
-  const passwordErrorelement = screen.queryByText(/the password you entered should contain 5 or more characters/i);
-  const passwordInputElement = screen.getByLabelText("Password");
-  const submitBtnElement = screen.getByRole("button", {
-    name: /submit/i
-  });
-
-
-  userEvent.type(emailInputElement, "blabla@mail.com");
-  expect(passwordErrorelement).not.toBeInTheDocument();
-  userEvent.type(passwordInputElement, "123");
-  userEvent.click(submitBtnElement);
-  const passwordErrorelement2 = screen.queryByText(/the password you entered should contain 5 or more characters/i);
-  expect(passwordErrorelement2).toBeInTheDocument();
-});
-
-test('should show confirm-password error if passwords dont match', () => {
-  render(<App />);
-  const emailInputElement = screen.getByRole("textbox", {
-    name: /email/i
-  });
-  const confirmPasswordErrorelement = screen.queryByText(/the passwords don't match. try again/i);
-  const passwordInputElement = screen.getByLabelText("Password");
-  const submitBtnElement = screen.getByRole("button", {
-    name: /submit/i
-  });
   const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i);
 
 
-  userEvent.type(emailInputElement, "blabla@mail.com");
-  userEvent.type(passwordInputElement, "12345");
-  expect(confirmPasswordErrorelement).not.toBeInTheDocument();
-  userEvent.type(confirmPasswordInputElement, "12347");
-  userEvent.click(submitBtnElement);
-  const confirmPasswordErrorelement2 = screen.queryByText(/the passwords don't match. try again/i);
-  expect(confirmPasswordErrorelement2).toBeInTheDocument();
+  if (email) {
+    userEvent.type(emailInputElement, email);
+  }
+  if (password) {
+    userEvent.type(passwordInputElement, password)
+  }
+  if (confirmPassword) {
+    userEvent.type(confirmPasswordInputElement, confirmPassword);
+  }
+  return {
+    emailInputElement,
+    passwordInputElement,
+    confirmPasswordInputElement
+  }
+};
+
+const clickOnSubmitBtn = () => {
+  userEvent.click(screen.getByRole("button", {
+    name: /submit/i
+  }));
+};
+
+describe('Error handling', () => {
+  test('should show email error message on invalid email', () => {
+    render(<App />);
+    expect(screen.queryByText(/the email you input is invalid/i)).not.toBeInTheDocument();
+    typeIntoForm({email:"blablamail.com"});
+    clickOnSubmitBtn();
+    expect(screen.getByText(/the email you input is invalid/i)).toBeInTheDocument();
+  });
+
+  test('should show password error if password is less than 5 characters', () => {
+    render(<App />);
+    typeIntoForm({email:"blabla@mail.com"});
+    expect(screen.queryByText(/the password you entered should contain 5 or more characters/i)).not.toBeInTheDocument();
+    typeIntoForm({password:"123"});
+    clickOnSubmitBtn();
+    expect(screen.getByText(/the password you entered should contain 5 or more characters/i)).toBeInTheDocument();
+  });
+
+  test('should show confirm-password error if passwords dont match', () => {
+    render(<App />);
+    typeIntoForm({email:"blabla@mail.com", password: "12345", confirmPassword: "12347"});
+    expect(screen.queryByText(/the passwords don't match. try again/i)).not.toBeInTheDocument();
+    clickOnSubmitBtn();
+    expect(screen.getByText(/the passwords don't match. try again/i)).toBeInTheDocument();
+  });
+
+  test('should show no error message if every input is valid', () => {
+    render(<App />);
+
+    typeIntoForm({email:"blabla@mail.com", password:"12345", confirmPassword: "12345"});
+    clickOnSubmitBtn();
+
+    expect(screen.queryByText(/the email you input is invalid/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/the password you entered should contain 5 or more characters/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/the passwords don't match. try again/i)).not.toBeInTheDocument();
+  });
 });
 
-test('should show no error message if every input is valid', () => {
-  render(<App />);
-  const emailInputElement = screen.getByRole("textbox", {
-    name: /email/i
+describe('input fields exist and working', () => {
+  test('inputs should be initially empty', () => {
+    render(<App />);
+    expect(screen.getByRole("textbox").value).toBe("");
+    expect(screen.getByLabelText("Password").value).toBe("");
+    expect(screen.getByLabelText(/confirm password/i).value).toBe("");
   });
-  const passwordInputElement = screen.getByLabelText("Password");
-  const submitBtnElement = screen.getByRole("button", {
-    name: /submit/i
+
+  test('should be able to type an email', () => {
+    render(<App />);
+    const {emailInputElement} = typeIntoForm({email: "bla@bla.com"});
+    expect(emailInputElement.value).toBe("bla@bla.com")
   });
-  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i);
 
-  userEvent.type(emailInputElement, "blabla@mail.com");
-  userEvent.type(passwordInputElement, "12345");
-  userEvent.type(confirmPasswordInputElement, "12345");
-  userEvent.click(submitBtnElement);
+  test('should be able to type a password', () => {
+    render(<App />);
+    const {passwordInputElement} = typeIntoForm({password: "pwd"});
+    expect(passwordInputElement.value).toBe("pwd")
+  });
 
-  const emailErrorelement = screen.queryByText(/the email you input is invalid/i);
-  const passwordErrorelement = screen.queryByText(/the password you entered should contain 5 or more characters/i);
-  const confirmPasswordErrorelement = screen.queryByText(/the passwords don't match. try again/i);
-
-  expect(emailErrorelement).not.toBeInTheDocument();
-  expect(passwordErrorelement).not.toBeInTheDocument();
-  expect(confirmPasswordErrorelement).not.toBeInTheDocument();
+  test('should be able to type a confirm-password', () => {
+    render(<App />);
+    const {confirmPasswordInputElement} = typeIntoForm({confirmPassword: "cpwd"});
+    expect(confirmPasswordInputElement.value).toBe("cpwd")
+  });
 });
