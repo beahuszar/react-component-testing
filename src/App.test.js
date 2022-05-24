32475ee334c3de1ebeeea6deb444571a8ff6c29e
Beata Huszar <beata.huszar@playngo.com>
2022-05-24 15:40:54 +0200

    add userEvent tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
-test('', () => {
+test('inputs should be initially empty', () => {
   render(<App />);
   const emailInputElement = screen.getByRole("textbox");
   const passwordInputElement = screen.getByLabelText("Password");
@@ -9,4 +10,27 @@ test('', () => {
   expect(emailInputElement.value).toBe("");
   expect(passwordInputElement.value).toBe("");
   expect(confirmPasswordInputElement.value).toBe("");
+});
+
+test('should be able to type an email', () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox", {
+    name: /email/i
+  });
+  userEvent.type(emailInputElement, "selena@gmail.com");
+  expect(emailInputElement.value).toBe("selena@gmail.com")
+});
+
+test('should be able to type a password', () => {
+  render(<App />);
+  const passwordInputElement = screen.getByLabelText("Password");
+  userEvent.type(passwordInputElement, "pwd");
+  expect(passwordInputElement.value).toBe("pwd")
+});
+
+test('should be able to type a confirm-password', () => {
+  render(<App />);
+  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i);
+  userEvent.type(confirmPasswordInputElement, "cpwd");
+  expect(confirmPasswordInputElement.value).toBe("cpwd")
 });
